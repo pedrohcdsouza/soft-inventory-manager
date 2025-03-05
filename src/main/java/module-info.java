d51eef8903874c3ae1f58a258c6135815c09f57f
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-03-05 15:44:05 -0300

    Adding the exports for javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module pedrohcdsouza.softinventorymanager {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
 
-
     opens pedrohcdsouza.softinventorymanager to javafx.fxml;
+    opens pedrohcdsouza.softinventorymanager.controllers to javafx.fxml;
+    opens pedrohcdsouza.softinventorymanager.models to javafx.base;
     exports pedrohcdsouza.softinventorymanager;
 }
