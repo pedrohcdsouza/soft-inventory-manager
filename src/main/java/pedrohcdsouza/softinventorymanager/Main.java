2a52f7a5863702be9cedd201ba3bf4bd70904178
Pedro Henrique Cardoso de Souza <pedrohcsousa8@hotmail.com>
2025-03-05 15:45:07 -0300

    Adding func to starts the code

--- src/main/java/pedrohcdsouza/softinventorymanager/Main.java
+++ src/main/java/pedrohcdsouza/softinventorymanager/Main.java
@@ -1,5 +1,27 @@
 package pedrohcdsouza.softinventorymanager;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @SuppressWarnings("exports")
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/index.fxml"));
+        Parent root = loader.load();
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("Gerenciador de Estoque");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
